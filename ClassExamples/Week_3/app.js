be13c0b98753bb0815168e9f44873c2334b35b87
JJuarez00 <120279816+JJuarez00@users.noreply.github.com>
2024-09-10 16:57:17 -0400

    Class Example Finished.

--- ClassExamples/Week_3/app.js
+++ ClassExamples/Week_3/app.js
@@ -1,20 +1,126 @@
 
+// Function One
 function simpleFunctionOne() {
-    // code
-    document.getElementById('changeTxt').innerHTML +=
-    "'DONT CHANGE THIS PARAGRAPH'";
+    document.getElementById("changeTxt").innerHTML +=
+      "<br/> <br/>"+"'DONT CHANGE THIS PARAGRAPH'";
 }
 
 simpleFunctionOne();
 
+// Function Two
 function simpleFunctionTwo() {
     document.querySelector("body").innerHTML += "This function does not take any parameters";
 }
 
 simpleFunctionTwo();
 
+
+// Function Two
 function sayHelloWorld() {
     console.log("Hello World! This is function Two.");
 }
 
 sayHelloWorld();
+
+
+
+// Function Three
+function sayFavBand(bandName) {
+    document.getElementById("favBand").innerHTML += bandName + "<br/>";
+}
+
+sayFavBand("The Marias");
+sayFavBand("Green Day");
+
+
+
+// Function Four
+function sayMyName(miNombre) {
+    console.log(miNombre);
+}
+
+sayMyName("Say my name");
+sayMyName("Say my name, luh bro");
+
+
+
+// Function Five
+function calcRecPerimeter(height, width) {
+  const perimeter = 2 * height + 2 * width;
+  document.querySelector("#panswer").innerHTML += perimeter;
+  // The # is used to call the ID, which is: id="panswer"
+}
+
+calcRecPerimeter(2, 4);
+
+// Function Six
+    // Make sure the function name is
+    // the same as the onclick="example()"
+function calcArea() {
+    const height = document.getElementById("height").value;
+    const width = document.getElementById("width").value;
+    console.log("height", height);
+    console.log("weight", width);
+    document.querySelector("#answer").innerHTML += "Area: " + height * width + "<br/>";
+}
+
+
+// Function Seven
+function calcRecAreaAndPeri(height, width) {
+    const perimeter = 2 * (height, width);
+    const area = height * width;
+
+    document.getElementById("multiAnswer").innerHTML += `
+    Height: ${height} <br/>
+    Width: ${width} <br/>
+    Perimeter: ${perimeter} <br/>
+    Area: ${area} <br/> <br/>
+    `;
+}
+
+calcRecAreaAndPeri(2, 2);
+calcRecAreaAndPeri(3, 2);
+
+
+// Function Eighth
+function myFunction() {
+    alert("Who Goes There?");
+}
+
+
+
+// Function Nine
+function wizard(name, occupation) {
+    confirm(`
+        Welcome ${name}, the ${occupation}
+        `)
+        
+    document.getElementById("future").innerHTML += `
+    Welcome ${name}, the ${occupation}!
+    `
+}
+
+
+// Function Ten
+function addItem() {
+    const newItem = prompt("Add Item: ");
+    console.log(newItem);
+}
+
+
+// Function Eleven
+
+function returnValue(color) {
+    return color;
+}
+
+alert(returnValue("blue"));
+
+
+// Function Tweleve
+function  calcCircleArea(radius) {
+    return 2* Math.PI * radius
+}
+
+document.getElementById("useOne").innerHTML += calcCircleArea(2);
+document.getElementById("useTwo").innerHTML += calcCircleArea(4);
