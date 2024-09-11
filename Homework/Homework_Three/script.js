e8c6ebca8c7e1efd20c5753896abb75f4142d5e5
Joseph <josephjuarez455@gmail.com>
2024-09-10 23:55:05 -0400

    Added Week 3 Homework

---
+++ Homework/Homework_Three/script.js
@@ -0,0 +1,24 @@
+
+function getTotal() {
+
+    clearText("answer")
+    let principle = parseFloat(document.getElementById("p").value);
+    let rate = parseFloat(document.getElementById("r").value);
+    let time = parseFloat(document.getElementById("t").value);
+
+    let totalInterest = (principle * rate * time) / 100;
+    let grandTotal = principle + totalInterest;
+
+
+    document.querySelector("#answer").innerHTML = 
+        `
+            With a beginning Principle of $${principle} and 
+            with a growth rate of ${rate}% for ${time}
+            year(s), your total interest will be $${totalInterest.toFixed(2)} 
+            with a grand total of $${grandTotal.toFixed(2)}
+        `;
+}
+
+function clearText(id) {
+    document.getElementById(id).innerHTML = "";
+}
