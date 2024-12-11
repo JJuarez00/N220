47009d62c9707d4bb8401e917cd2af987eb64917
JJuarez00 <120279816+JJuarez00@users.noreply.github.com>
2024-12-10 20:50:59 -0500

    Updates to final project.

--- FINAL/app.js
+++ FINAL/app.js
@@ -1,36 +1,66 @@
-const users = [];
+// store users in localStorage
+if (!localStorage.getItem("users")) {
+    localStorage.setItem("users", JSON.stringify([]));
+}
 
+// create user button
 function createUser() {
-    const username = document.getElementById("newUsername").value.trim();
-    const password = document.getElementById("newPassword").value.trim();
+    const newUsername = document.getElementById("newUsername").value.trim();
+    const newPassword = document.getElementById("newPassword").value.trim();
+    const message = document.getElementById("message");
 
+    // fetch existing users from localStorage
+    const users = JSON.parse(localStorage.getItem("users"));
 
-    // Check if the username exists in the system
-    const usernameExists = users.some(user => user.username === username);
+    // clear text
+    message.textContent = "";
 
-    if (usernameExists) {
-        alert("This username already exists. Please choose a different username.");
+    // check if the username already exists
+    if (usernameExists = users.some(user => user.username === newUsername)) {
+        message.textContent = "This username already exists. Please choose a different username.";
         document.getElementById("newUsername").value = "";
         document.getElementById("newPassword").value = "";
-
         return;
     }
 
+    // add the new user
     const user = {
-        username: username,
-        password: password,
+        username: newUsername,
+        password: newPassword,
     };
-    
+
+    // add user to users list
     users.push(user);
+
+    // save updated users list to localStorage
+    localStorage.setItem("users", JSON.stringify(users));
 
     // clear the input fields
     document.getElementById("newUsername").value = "";
     document.getElementById("newPassword").value = "";
+
+    message.textContent = "Account successfully created!";
     
-    // display the updated user list in the console for testing
-    console.log(users);
+    console.log(users); // for debugging, remove later
 }
 
+// login Button
 function login() {
-    // check if user login exist if so load html page
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const message = document.getElementById("message");
+
+    // clear text
+    message.textContent = "";
+
+    // fetch users from localStorage
+    const users = JSON.parse(localStorage.getItem("users"));
+
+    // check if user exists and check if passwords align
+    if (users.find(user => user.username === username && user.password === password)) {
+        
+        window.location.href = "dashboard.html";
+    } else {
+        message.textContent = "Invalid username or password. Please try again.";
+    }
 }
